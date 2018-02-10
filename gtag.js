bce58b4169a13c534a7b2fac7006ca7d0b0d9b3c
YIFEI <1007255835@qq.com>
2018-02-10 10:49:11 +0800

    Site updated: 2018-02-10 10:49:04

--- gtag.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-89376999-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-89376999-1');
-</script>
